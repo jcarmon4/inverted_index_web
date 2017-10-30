c90642e2ca4ea77219f625fcc0dbb3ce5f0a5af5
Alejocram <alejocram@gmail.com>
2017-10-30 11:02:52 -0500

    Rewriting code with Camelcase notation.

--- routes/app.js
+++ routes/app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var redis = require('redis');
 
-var redisClient = redis.createClient(process.env.REDIS_URL);
+var redisClient = redis.createClient("redis://h:padd1089bb3eef4b1bf8c5cd5019461d8f7ad76b4c6960640f882ce0f2a9c86a6@ec2-34-224-49-43.compute-1.amazonaws.com:65139");
 redisClient.select(1);
+//var redisClient = redis.createClient({host:'localhost', port: '6379', db: 7});
 
 redisClient.on('ready',function() {
     console.log("Redis is ready");
@@ -16,6 +17,140 @@ redisClient.on('error',function() {
 router.get('/', function (req, res, next) {
     res.render('index');
 });
+
+router.get('/keyspro', function (req, res, next) {
+    getKeysPromise.then(function (keys) {
+        keys.forEach(function (key, i) {
+            console.log(key);
+        });
+    }, console.err);
+});
+
+router.get('/search/:q', function (req, res, next) {
+    var query = req.params.q;
+    var words = query.split("+");
+    var wordsInQuery = [];
+    words.forEach(function(word, i) {
+        wordsInQuery.push("word:"+word);
+    });
+
+    var wordWithQuantityMap = new Map();
+    words.forEach(function (word1) {
+        var count = 0;
+        words.forEach(function (word2) {
+            if (word1 === word2){
+                count += 1;
+            }
+        });
+        wordWithQuantityMap.set(word1, count);
+    });
+
+    //TF is calculated for every unique word of the query
+    var wordWithTfMap = new Map();
+    words.forEach(function (word) {
+        wordWithTfMap.set(word, wordWithQuantityMap.get(word) / words.length);
+        console.log(word+" "+wordWithTfMap.get(word));
+    });
+
+    //TfIdf is calculated for every unique word of the query
+    var wordWithIdfMap = new Map();
+    var wordWithTfidfMap = new Map();
+    words.forEach(function (word, i) {
+        //Requesting word's idf by word
+        getIDFbyDoc(word).then(function (idf) {
+            wordWithIdfMap.set(word, idf);
+            console.log("IDF");
+            console.log(i+" word: "+word+" "+idf);
+            wordWithTfidfMap.set(word, wordWithTfMap.get(word) * idf);
+
+            if ((i+1) === words.length){
+                console.log("TFIDF");
+                console.dir(wordWithTfidfMap);
+                var queryMagnitude = calculateQueryMagnitude(wordWithTfidfMap);
+                console.log("Query magnitude "+queryMagnitude);
+
+                var uniqueDocsSet = new Set();
+                wordsInQuery.forEach(function (word, j) {
+                    getDocsByWord(word).then(function(docs) {
+                        docs.forEach(function (doc) {
+                            uniqueDocsSet.add(doc);
+                        });
+                        if ((j+1) === wordsInQuery.length){
+                            console.log("Unique Docs");
+                            console.dir(uniqueDocsSet);
+
+                            dotProduct(uniqueDocsSet, wordWithTfidfMap, queryMagnitude);
+                        }
+                    }).catch(function (error) {
+                        console.log("Promise getDocsByWord Rejected");
+                        console.error(error);
+                    });
+                });
+            }
+        }).catch(function () {
+            console.log("Promise getIDFbyDoc Rejected");
+        });
+    });
+});
+
+function calculateQueryMagnitude(wordWithTfidfMap) {
+    var sum = 0;
+    wordWithTfidfMap.forEach(function(value, key) {
+        sum += Math.pow(value, 2);
+    });
+    return Math.sqrt(sum);
+}
+
+function dotProduct(uniqueDocsSet, wordWithTfidfMap, queryMagnitude){
+    var docNum = 0;
+    var similarityByDoc = [];
+    if (uniqueDocsSet.size > 0) {
+
+        uniqueDocsSet.forEach(function (doc) {
+            var resNum = 0;
+            var dotProduct = 0;
+            console.log("doc: " + doc);
+            wordWithTfidfMap.forEach(function (tfidfQuery, wordQuery) {
+                console.log("word_q:" + wordQuery + " tfidf_q:" + tfidfQuery);
+                getTFIDFFromService(doc, wordQuery).then(function (tfidf) {
+                    resNum += 1;
+                    console.log("doc:" + doc + " word_q:" + wordQuery + " tfidf_q:" + tfidfQuery + " tfidf:" + tfidf);
+                    dotProduct += (tfidfQuery * tfidf);
+
+                    if (wordWithTfidfMap.size === resNum) {
+                        console.log("dotProduct:" + dotProduct);
+                        getMagnitudeFromService(doc).then(function (magnitude) {
+                            docNum += 1;
+                            console.log("magnitude " + doc + ": " + magnitude);
+                            console.log("magnitude Q " + queryMagnitude);
+                            console.log("dotProduct " + dotProduct);
+                            var similarity = dotProduct / (magnitude * queryMagnitude);
+                            console.log("doc " + doc + " similarity " + similarity);
+                            similarityByDoc.push([doc, similarity]);
+
+                            console.log("result_docs: " + uniqueDocsSet.size + " docNum: " + docNum)
+                            if (uniqueDocsSet.size === docNum) {
+                                console.log("Similarity sort");
+                                similarityByDoc.sort(function (a, b) {
+                                    return b[1] - a[1];
+                                });
+                                console.dir(similarityByDoc);
+                                //Send to a function(similarityByDoc);
+                            }
+                        });
+                    }
+                }).catch(function (error) {
+                    console.log("Promise Rejected getTFIDFFromService");
+                    console.error(error);
+                });
+            });
+        });
+    } else {
+        console.log("There aren't docs");
+        // There are no docs to examine.
+        //Send to a function(similarityByDoc);
+    }
+}
 
 var getIDFbyDoc = function (word) {
     return new Promise(function (resolve, reject) {
@@ -71,148 +206,9 @@ getKeysPromise = new Promise(function (resolve, reject) {
     });
 });
 
-router.get('/keyspro', function (req, res, next) {
-    getKeysPromise.then(function (keys) {
-        keys.forEach(function (key, i) {
-            console.log(key);
-        });
-    }, console.err);
-});
-
-function getMagnitude(tfidf_by_word) {
-    var sum = 0;
-    tfidf_by_word.forEach(function(value, key) {
-        sum += Math.pow(value, 2);
-    });
-    return Math.sqrt(sum);
-}
-
-router.get('/search/:q', function (req, res, next) {
-    var query = req.params.q;
-    var words = query.split("+");
-    var words_to_search = [];
-    words.forEach(function(word, i) {
-        words_to_search.push("word:"+word);
-    });
-    var word_counts_to_search = words_to_search.length;
-
-    var count_of_word_in_array = new Map();
-    words.forEach(function (word_i) {
-        var count = 0;
-        words.forEach(function (word_j) {
-            if (word_i === word_j){
-                count += 1;
-            }
-        });
-        count_of_word_in_array.set(word_i, count);
-    });
-
-    var tf_by_word = new Map();
-    words.forEach(function (word) {
-        tf_by_word.set(word, count_of_word_in_array.get(word) / words.length);
-        console.log(word+" "+tf_by_word.get(word));
-    });
-
-    var idf_by_word = new Map();
-    var tfidf_by_word = new Map();
-    var num_response = 0;
-    words.forEach(function (word, i) {
-        getIDFbyDoc(word).then(function (idf) {
-            num_response += 1;
-            idf_by_word.set(word, idf);
-            console.log("IDF");
-            console.log("word: "+word+" "+idf)
-            tfidf_by_word.set(word, tf_by_word.get(word) * idf);
-
-            if (num_response === words.length){
-                console.log("TFIDF");
-                console.dir(tfidf_by_word);
-                var magnitude = getMagnitude(tfidf_by_word);
-                console.log("Magnitude "+magnitude);
-
-
-                var result_counts = 0;
-                var result_docs = new Set();
-                words_to_search.forEach(function (word, i) {
-                    getDocsByWord(word).then(function(docs) {
-                        result_counts += 1;
-                        docs.forEach(function (doc, i) {
-                            result_docs.add(doc);
-                        });
-                        if (result_counts == word_counts_to_search){
-                            console.log("Docs");
-                            console.dir(result_docs);
-
-                            dotProduct(result_docs, tfidf_by_word, magnitude);
-                        }
-                    }).catch(function (error) {
-                        console.log("Promise Rejected getDocsByWord");
-                        console.error(error);
-                    });
-                });
-            }
-        }).catch(function () {
-            console.log("Promise Rejected");
-        });
-    });
-});
-
-function dotProduct(result_docs, tfidf_by_word, magnitude_q){
-    var docNum = 0;
-    var similarityByDoc = [];
-    if (result_docs.size > 0) {
-
-        result_docs.forEach(function (doc) {
-            var resNum = 0;
-            var dotProduct = 0;
-            console.log("doc: " + doc);
-            tfidf_by_word.forEach(function (tfidf_q, word_q) {
-                console.log("word_q:" + word_q + " tfidf_q:" + tfidf_q);
-                getTFIDFFromService(doc, word_q).then(function (tfidf) {
-                    resNum += 1;
-                    console.log("doc:" + doc + " word_q:" + word_q + " tfidf_q:" + tfidf_q + " tfidf:" + tfidf);
-                    dotProduct += (tfidf_q * tfidf);
-
-                    if (tfidf_by_word.size === resNum) {
-                        console.log("dotProduct:" + dotProduct);
-                        getMagnitudeFromService(doc).then(function (magnitude) {
-                            docNum += 1;
-                            console.log("magnitude " + doc + ": " + magnitude);
-                            console.log("magnitude Q " + magnitude_q);
-                            console.log("dotProduct " + dotProduct);
-                            var similarity = dotProduct / (magnitude * magnitude_q);
-                            console.log("doc " + doc + " similarity " + similarity);
-                            similarityByDoc.push([doc, similarity]);
-
-                            console.log("result_docs: " + result_docs.size + " docNum: " + docNum)
-                            if (result_docs.size === docNum) {
-                                console.log("Similarity sort");
-                                similarityByDoc.sort(function (a, b) {
-                                    return b[1] - a[1];
-                                });
-                                console.dir(similarityByDoc);
-                                //Send to a function(similarityByDoc);
-                            }
-                        });
-                    }
-                }).catch(function (error) {
-                    console.log("Promise Rejected getTFIDFFromService");
-                    console.error(error);
-                });
-            });
-        });
-    } else {
-        console.log("There aren't docs");
-        // There are no docs to examine.
-        //Send to a function(similarityByDoc);
-    }
-}
-
-
-
-var getTFIDFFromService = function (doc, word_q) {
+var getTFIDFFromService = function (doc, queryWord) {
     return new Promise(function (resolve, reject) {
-        var key = "tfidf:"+doc+";;"+word_q;
+        var key = "tfidf:"+doc+";;"+queryWord;
         console.log(key);
         redisClient.get(key, function (err, value) {
             if(err){
